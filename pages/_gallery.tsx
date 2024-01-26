e6832e9975d95a89d6d832faf2034dbd0ffd5ea0
Nhordejan <nhordejan@gmail.com>
2024-01-26 17:16:51 +1000

    updated ImageGalleryProps to have optional list of images

--- pages/_gallery.tsx
+++ pages/_gallery.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image';
 import Modal from 'react-modal';
 
 interface ImageGalleryProps {
-  images: string[];
+  images?: string[]; // Make the images prop optional
 }
 
-const ImageGallery: React.FC<ImageGalleryProps> = ({ images }) => {
+const ImageGallery: React.FC<ImageGalleryProps> = ({ images = [] }) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
@@ -22,41 +22,41 @@ const ImageGallery: React.FC<ImageGalleryProps> = ({ images }) => {
 
   return (
     <div className="image-gallery-container">
-      {images.map((imageName, index) => (
+      {images && images.map((imageName, index) => (
         <div key={index} className="image-container" onClick={() => openModal(imageName)}>
           <Image
             src={`/images/${imageName}`}
             alt={`Image ${index + 1}`}
             layout="fixed"
-            width={200} // Set a fixed width for the images
-            height={100} // Set a fixed height for the images
+            width={200}
+            height={100}
             objectFit="cover"
             loading="lazy"
           />
         </div>
       ))}
 
-          <Modal
-            isOpen={modalIsOpen}
-            onRequestClose={closeModal}
-            contentLabel="Image Modal"
-            shouldCloseOnOverlayClick={true}
-            style={{
-              content: {
-                width: '50%',
-                height: '50%',
-                margin: 'auto',
-              },
-            }}
-          >
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Image Modal"
+        shouldCloseOnOverlayClick={true}
+        style={{
+          content: {
+            width: '50%',
+            height: '50%',
+            margin: 'auto',
+          },
+        }}
+      >
         {selectedImage && (
           <div>
             <Image
               src={`/images/${selectedImage}`}
               alt="Selected Image"
               layout="responsive"
-              width={800} // Set a suitable width for the modal image
-              height={800} // Set a suitable height for the modal image
+              width={800}
+              height={800}
               objectFit="contain"
             />
           </div>
