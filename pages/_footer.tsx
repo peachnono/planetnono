71c2a7bcba60caca7ee1689ae85853748fdccaf3
Nix <nhordejan@gmail.com>
2024-05-09 15:52:51 +1000

    added creative coding stuff

--- pages/_footer.tsx
+++ pages/_footer.tsx
@@ -10,8 +10,8 @@ const Footer: React.FC = () => {
             <p className={styles.babydollfont}>Lets make something awesome! Reach me at <a href='#'>nhordejan@gmail.com</a>
 
             </p>
-              <a href='#'><img src="/images/linkedin.png" alt="LinkedIn" /></a>
-              <a href='#'><img src="/images/github.png" alt="GitHub" /></a>
+              <a href='https://www.linkedin.com/public-profile/settings?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_self_edit_contact-info%3BpQDyx8qPRheqgACmRQgvTA%3D%3D'><img src="/images/linkedin.png" alt="LinkedIn" /></a>
+              <a href='https://github.com/peachnono'><img src="/images/github.png" alt="GitHub" /></a>
               <a href='#'><img src="/images/discord.png" alt="Discord" /></a>
           </div>
       </div>
--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -20,9 +20,21 @@ const Portfolio: React.FC = () => {
         </div>
         <div className='STIMulate'>
         <h2>STIMulate Workshop Resource (Present)</h2>
-        <p className='babydollFont'>This is a site I made as a supplementary resource for the workshops of STIMulate. You can visit the site here: <a href='https://helpifb104.github.io/'>https://helpifb104.github.io/</a></p>
+        <p className='babydollFont'>This is a site I made as a supplementary resource for the workshops of STIMulate that I do as a Peer Learning Facilitator. You can visit the site here: <a href='https://helpifb104.github.io/'>https://helpifb104.github.io/</a></p>
         <div className='STIMulateImages'>
         <img src="/images/Workshop.png" alt="Code Network" />
+        </div>
+        <div className='creativeCoding'></div>
+        <h2>Creative Coding with P5js (Present)</h2>
+        <p className='babydollFont'>One of the units I'm taking in my current semester is called "Creative Coding" led by Jared Donovan as unit coordinator. Below is a demo of the work I've done for the class. You 
+        can also use this link to find my code and interact with it: <a href='https://editor.p5js.org/peachnono/collections/boJfU1ArD'>P5js Collection</a>.</p>
+        <div className='iframeContainer'>
+        <video width="600" height="400" controls>
+        <source src="/images/ccoding.mp4" type="video/mp4" />
+        </video>
+        <video width="600" height="400" controls>
+        <source src="/images/ccoding2.mp4" type="video/mp4" />
+        </video>
         </div>
         </div>
     </div>
